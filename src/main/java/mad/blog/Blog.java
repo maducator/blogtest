6f956eb988ac847ba38c7ce14c0c39047706ea87
madstorm.lee <madstorm.lee@gmail.com>
2022-12-01 13:51:47 +0900

    Contstructor가 없어서 에러가 난다고 한다.

--- src/main/java/mad/blog/Blog.java
+++ src/main/java/mad/blog/Blog.java
@@ -8,11 +8,15 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
 public class Blog {
     @Id
